53e20ec57b7a15632490126116f3d3ec4b341966
Juunibyou <danijunn@gmail.com>
2025-12-07 20:33:42 -0600

    junit

--- app/src/test/java/org/example/AppTest.java
+++ app/src/test/java/org/example/AppTest.java
@@ -4,6 +4,23 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class AppTest {
-    @Test void appHasAGreeting() {
+
+    @Test
+    void testSetPixel() {
+        Canvas canvas = new Canvas(10, 10);
+        canvas.setPixel(2, 3, 'X');
+
+        assertEquals('X', canvas.getPixel(2, 3));
     }
+
+    @Test
+    void testSetPixelInvalid() {
+        Canvas canvas = new Canvas(10, 10);
+
+        canvas.setPixel(99, 99, 'X');
+
+        assertEquals('.', canvas.getPixel(0, 0)); 
+    }
+
+
 }
