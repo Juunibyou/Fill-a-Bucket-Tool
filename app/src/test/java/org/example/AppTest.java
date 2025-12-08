918cb95b78204f3e4cf40cd350b75a46be6ceeeb
Juunibyou <danijunn@gmail.com>
2025-12-07 20:37:37 -0600

    junit 2

--- app/src/test/java/org/example/AppTest.java
+++ app/src/test/java/org/example/AppTest.java
@@ -22,5 +22,33 @@ class AppTest {
         assertEquals('.', canvas.getPixel(0, 0)); 
     }
 
+    @Test
+    void testSimpleFill() {
+        Canvas canvas = new Canvas(5, 5);
 
+        canvas.setPixel(1, 1, 'A');
+        canvas.setPixel(1, 2, 'A');
+        canvas.setPixel(2, 2, 'A');
+
+        BucketFill.floodFill(canvas, 1, 1, 'Z');
+
+        assertEquals('Z', canvas.getPixel(1, 1));
+        assertEquals('Z', canvas.getPixel(1, 2));
+        assertEquals('Z', canvas.getPixel(2, 2));
+    }
+
+    @Test
+    void testFillStopsAtDifferentColor() {
+        Canvas canvas = new Canvas(5, 5);
+
+        canvas.setPixel(0, 0, 'A');
+        canvas.setPixel(1, 0, 'A');
+        canvas.setPixel(2, 0, 'B');
+
+        BucketFill.floodFill(canvas, 0, 0, 'X');
+
+        assertEquals('X', canvas.getPixel(0, 0));
+        assertEquals('X', canvas.getPixel(1, 0));
+        assertEquals('B', canvas.getPixel(2, 0));
+    }
 }
